e08732516ec173f439e4af54f182cb5d2e4648ef
Tariq Mostahid <tariqmostahid@gmail.com>
2021-04-19 17:56:32 +0600

    Update Hello function. now can take a argument. also updateed test function.

--- go_basic/hello_test.go
+++ go_basic/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
